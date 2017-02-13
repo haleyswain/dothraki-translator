5c8af31afa6b280a11a3d3bdeeb899a4143ad4f5
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-02-13 10:14:21 -0800

    Update API key to deploy

--- js/game-interface.js
+++ js/game-interface.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     var input = $('#phrase').val();
     $('#phrase').val("");
     $('.showPhrase').text('Translation:' + input + ".");
-    $.post('http://api.funtranslations.com/translate/dothraki.json?text=' + input).then(function(response) {
+    $.post('https://api.funtranslations.com/translate/dothraki.json?text=' + input).then(function(response) {
       $('.showTranslation').append(response.contents.translated);
     });
   });
